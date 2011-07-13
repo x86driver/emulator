79e1c5422807e2e73f8e8cb1b3b00ed0b4721a51
doremi <doremi@ubuntu>
2011-07-13 11:18:32 +0800

    對 ldr r0, [pc] 特別處理

--- reg.h
+++ reg.h
@@ -7,5 +7,12 @@ char *reg_name(int reg);
 uint32_t get_reg(struct CPUState *env, int reg);
 void set_reg(struct CPUState *env, int reg, uint32_t val);
 
+enum {
+    REG_R0 = 0, REG_R1, REG_R2, REG_R3,
+    REG_R4, REG_R5, REG_R6, REG_R7,
+    REG_R8, REG_R9, REG_SL, REG_FP,
+    REG_IP, REG_SP, REG_LR, REG_PC
+};
+
 #endif
 
