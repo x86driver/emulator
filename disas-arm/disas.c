582a8cf3fc8f65fa1d036d7c18923fd93d57ea8b
Shane Lin <253692+doremi@users.noreply.github.com>
2011-07-25 17:22:55 +0800

    把 qemu-0.14 的 ARM disassembler 移植過來
    同時加上 getopt 參數, 可用 -d 印出組語

---
+++ disas-arm/disas.c
@@ -0,0 +1,58 @@
+/* General "disassemble this chunk" code.  Used for debugging. */
+//#include "config.h"
+#include "dis-asm.h"
+#include "elf.h"
+#include <errno.h>
+
+/* Filled in by elfload.c.  Simplistic, but will do for now. */
+struct syminfo *syminfos = NULL;
+
+/* Get LENGTH bytes from info's buffer, at target address memaddr.
+   Transfer them to myaddr.  */
+int
+buffer_read_memory(bfd_vma memaddr, bfd_byte *myaddr, int length,
+                   struct disassemble_info *info)
+{
+    if (memaddr < info->buffer_vma
+        || memaddr + length > info->buffer_vma + info->buffer_length)
+        /* Out of bounds.  Use EIO because GDB uses it.  */
+        return EIO;
+    memcpy (myaddr, info->buffer + (memaddr - info->buffer_vma), length);
+    return 0;
+}
+
+/* Print an error message.  We can assume that this is in response to
+   an error return from buffer_read_memory.  */
+void
+perror_memory (int status, bfd_vma memaddr, struct disassemble_info *info)
+{
+  if (status != EIO)
+    /* Can't happen.  */
+    (*info->fprintf_func) (info->stream, "Unknown error %d\n", status);
+  else
+    /* Actually, address between memaddr and memaddr + len was
+       out of bounds.  */
+    (*info->fprintf_func) (info->stream,
+			   "Address 0x%" PRIx64 " is out of bounds.\n", memaddr);
+}
+
+/* This could be in a separate file, to save miniscule amounts of space
+   in statically linked executables.  */
+
+/* Just print the address is hex.  This is included for completeness even
+   though both GDB and objdump provide their own (to print symbolic
+   addresses).  */
+
+void
+generic_print_address (bfd_vma addr, struct disassemble_info *info)
+{
+    (*info->fprintf_func) (info->stream, "0x%" PRIx64, addr);
+}
+
+/* Just return the given address.  */
+
+int
+generic_symbol_at_address (bfd_vma addr, struct disassemble_info *info)
+{
+  return 1;
+}
---
+++ disas-arm/disas.h
@@ -0,0 +1,6 @@
+#ifndef DISAS_H
+#define DISAS_H
+
+void disas(FILE *out, void *code, unsigned long size);
+
+#endif
