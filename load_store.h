2b4997c6aac0d24414a48aa47a9257434ad2a6b0
doremi <doremi@ubuntu>
2011-08-12 15:10:27 +0800

    修正 mov (imm)
    * 原本 mov_imm 是用 expand_imm12
      但應該要用 expand_imm12_C
      因此再多寫一個 shift_C

--- load_store.h
+++ load_store.h
@@ -16,6 +16,7 @@ int multi_ldst(struct CPUState *env, uint32_t inst);
 
 uint32_t decode_imm_shift(uint32_t type, uint32_t imm5);
 uint32_t shift(struct CPUState *env, uint32_t val, uint32_t type, uint32_t imm5, int update);
+uint32_t shift_C(struct CPUState *env, uint32_t val, uint32_t type, uint32_t imm5, uint32_t *carry);
 const char *shift_type(uint32_t type, uint32_t imm5);
 
 #endif
